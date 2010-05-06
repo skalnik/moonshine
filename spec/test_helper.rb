d366bfa0724965602aad3ae49ea2e5f0d319587d
Joshua Nichols <josh@technicalpickles.com>
2010-05-06 00:39:04 -0400

    Remove assertions from test_helper.

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -35,17 +35,6 @@ Test::Unit::TestCase.class_eval do
     $here.join('generator_rails_root')
   end
 
-  def assert_manifest_file_exists(manifest, path)
-    message = "manifest (#{manifest.files.keys.join(', ')}) files does not contain #{path}"
-    assert_block message do
-      manifest.files.has_key?(path.to_s)
-    end
-  end
-
-  def assert_apache_directive(contents, directive, value)
-    contents.should have_apache_directive(directive, value)
-  end
-
   def in_apache_if_module(contents, some_module)
     contents.should =~ /<IfModule #{some_module}>(.*)<\/IfModule>/m
 
